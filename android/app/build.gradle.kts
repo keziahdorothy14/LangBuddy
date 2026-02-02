900ebb4aeff6ca205a7f9b246af1fabde1ec99d5
Keziah Dorothy A G <keziahdorothy14@gmail.com>
2026-02-02 13:54:41 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,52 +4,54 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-android {
-    namespace = "com.yourcompany.langbuddy"
-    compileSdk = 34
+import java.util.Properties
 
-    defaultConfig {
-        applicationId = "com.yourcompany.langbuddy"
-        minSdk = flutter.minSdkVersion
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-    }
+        android {
+            namespace = "com.yourcompany.langbuddy"
+            compileSdk = 34  // or use flutter.compileSdkVersion if you prefer
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    signingConfigs {
-        create("release") {
-            val keystorePropertiesFile = file("key.properties")
-            val keystoreProperties = java.util.Properties().apply {
-                load(keystorePropertiesFile.inputStream())
+            defaultConfig {
+                applicationId = "com.yourcompany.langbuddy"
+                minSdk = flutter.minSdkVersion
+                targetSdk = 34
+                versionCode = 1
+                versionName = "1.0.0"
             }
 
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-        }
-    }
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
 
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false   // set to true if you want code shrinking
-            isShrinkResources = false // set to true if you want resource shrinking
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_11.toString()
+            }
+
+            signingConfigs {
+                create("release") {
+                    val keystorePropertiesFile = rootProject.file("android/key.properties")
+                    val keystoreProperties = Properties().apply {
+                        load(keystorePropertiesFile.inputStream())
+                    }
+
+                    keyAlias = keystoreProperties["keyAlias"] as String
+                    keyPassword = keystoreProperties["keyPassword"] as String
+                    storeFile = file(keystoreProperties["storeFile"] as String)
+                    storePassword = keystoreProperties["storePassword"] as String
+                }
+            }
+
+            buildTypes {
+                getByName("release") {
+                    signingConfig = signingConfigs.getByName("release")
+                    isMinifyEnabled = false
+                    isShrinkResources = false
+                }
+                getByName("debug") {
+                    // optional: you can also sign debug builds
+                }
+            }
         }
-        getByName("debug") {
-            // use default debug signing
-        }
-    }
-}
 
 flutter {
     source = "../.."
